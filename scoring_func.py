89cd9efd21e89ef342ca7b9d87c78dd0a2cf590f
Prateek Kumar Jha <85632179+PKJ-x7@users.noreply.github.com>
2021-11-04 13:02:19 +0530

    Update scoring_func.py
    removed async, updated evaluation method

--- scoring_func.py
+++ scoring_func.py
@@ -7,49 +7,51 @@ import chess
 import re
 from tqdm import tqdm
 
-# Example input
+# e4 c5 2. c3 Nc6 3. d4 cxd4 4. cxd4 e6 5. Nf3 Bb4+ 6. Nc3 d6 7. Bd2 Nge7 8. d5 exd5
 src = ['e4', 'c5', 'c3', 'Nc6', 'd4', 'cxd4', 'cxd4', 'e6', 'Nf3', 'Bb4+', 'Nc3', 'd6', 'Bd2', 'Nge7', 'd5']
-trg = ['exd5']
+trg = ['Rad8']
 res = ['exd5', '<eos>']
 
 
 def stockfish14_scores(move_list, prediction):
 
+    score = []
     board_in = chess.Board()
-    # move_list has to be a list of whole moves.
-    # example: move_list = ['e4', 'c5', 'c3', 'Nc6', 'd4', 'cxd4', 'cxd4', 'e6', 'Nf3', 'Bb4+', 'Nc3', 'd6', 'Bd2', 'Nge7', 'd5']
     for move in move_list:
+      # illegal move in inital game seq
+      try:
         board_in.push_san(move)
+      except ValueError:
+        return score
     # use the input board with initial game state
-    # board = board_in
 
-    async def main() -> None:
-        transport, engine = await chess.engine.popen_uci("stockfish_14_x64_avx2.exe")
-        # Scoring ----> Mate(-0) < Mate(-1) < Cp(-50) < Cp(200) < Mate(4) < Mate(1) < MateGiven
+    engine = chess.engine.SimpleEngine.popen_uci("/content/drive/MyDrive/NLP-Chess_lang-model/stockfish_13_linux_x64_avx2")
+    # Scoring ----> Mate(-0) < Mate(-1) < Cp(-50) < Cp(200) < Mate(4) < Mate(1) < MateGiven
 
-        # using copy to have two diff boards for evaluation
-        eval_board = copy.deepcopy(board_in)
+    # using copy to have two diff boards for evaluation
+    eval_board = copy.deepcopy(board_in)
 
-        info = await engine.analyse(eval_board, chess.engine.Limit(time=0.1, depth=18))
-        print("Initial:", info["score"])
+    info = engine.analyse(eval_board, chess.engine.Limit(time=0.1))
+    score.append(info["score"].white().wdl().expectation())
+    # win-draw-lose percentage from white's perspective 
+    try:
+        board_in.push_san(prediction[0])
+    except ValueError:
+        # print("Illegal move")
+        score.append(float('-inf'))
+    else:
+        info = engine.analyse(board_in, chess.engine.Limit(time=0.1))
+        score.append(info['score'].white().wdl().expectation())
 
-        try:
-            board_in.push_san(prediction[0])
-        except ValueError:
-            print("Illegal move")
-        else:
-            info = await engine.analyse(board_in, chess.engine.Limit(time=0.1, depth=18))
-            print('Prediction:', info["score"])
+    result = engine.play(eval_board, chess.engine.Limit(time=0.1))
+    eval_board.push(result.move)
+    info = engine.analyse(eval_board, chess.engine.Limit(time=0.1))
+    score.append(info['score'].white().wdl().expectation())
 
-        result = await engine.play(eval_board, chess.engine.Limit(time=0.1, depth=18))
-        eval_board.push(result.move)
-        info = await engine.analyse(eval_board, chess.engine.Limit(time=0.1, depth=18))
-        print('Stockfish:', info["score"])
+    engine.quit()
+    return score
 
-        await engine.quit()
-
-    asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
-    asyncio.run(main())
-
-
-stockfish14_scores(src, res)
+# pred = ['err']
+score = stockfish14_scores(src, res)
+# +ve score --------> stockfish better
+# if score == inf  ------> illelagl move predicted
